652646e996beab73189f789ca0b3d91252f51def
LAPTOP-IKDU3139\ADMIN <hunglt5@vng.com.vn>
2020-09-05 01:23:15 +0700

    Updates

---
+++ precache-manifest.66d12627f01c6705d7f1ac6e94682682.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d78484edfc845d633f69f778853fc87",
+    "url": "/Reactjs-management/index.html"
+  },
+  {
+    "revision": "381af317eefd8b002c6f",
+    "url": "/Reactjs-management/static/css/main.8955d074.chunk.css"
+  },
+  {
+    "revision": "2c7c272d4a1e56fa54f3",
+    "url": "/Reactjs-management/static/js/2.5060756e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Reactjs-management/static/js/2.5060756e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "381af317eefd8b002c6f",
+    "url": "/Reactjs-management/static/js/main.3e47659d.chunk.js"
+  },
+  {
+    "revision": "3355289a06d7f08de046",
+    "url": "/Reactjs-management/static/js/runtime-main.148378f0.js"
+  }
+]);
